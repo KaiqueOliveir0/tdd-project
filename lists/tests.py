4f4a578a053a99f7ad166a6a72fdddd67e3147f8
Kaique Oliveira <kaique.bti@gmail.com>
2023-05-05 23:14:51 -0300

    Não Salvar Items em Branco a cada Requisição

--- lists/tests.py
+++ lists/tests.py
@@ -3,20 +3,25 @@ from django.shortcuts import render
 from django.test import TestCase
 from lists.models import Item
 
-def home_page(request):
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+class HomePageTest(TestCase):
+    def home_page(request):
+        return render(request, 'home.html', {
+            'new_item_text': request.POST.get('item_text', ''),
+        })
 
-def test_can_save_a_POST_request(self):
-    response = self.client.post('/', data={'item_text': 'A new list item'})
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
 
-    self.assertEqual(Item.objects.count(), 1)
-    new_item = Item.objects.first()
-    self.assertEqual(new_item.text, 'A new list item')
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
-    self.assertIn('A new list item', response.content.decode())
-    self.assertTemplateUsed(response, 'home.html')
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
 
