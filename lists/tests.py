64d1d20bfa001680ef81839dd9ad57c36b84dca4
Kaique Oliveira <kaique.bti@gmail.com>
2023-05-05 23:05:07 -0300

    Passando Variáveis no Template

--- lists/tests.py
+++ lists/tests.py
@@ -5,3 +5,8 @@ def home_page(request):
     if request.method == 'POST':
         return HttpResponse(request.POST['item_text'])
     return render(request, 'home.html')
+
+def test_can_save_a_POST_request(self):
+   response = self.client.post('/', data={'item_text': 'A new list item'})
+   self.assertIn('A new list item', response.content.decode())
+   self.assertTemplateUsed(response, 'home.html')
