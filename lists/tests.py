bb90f1786c7ff8b5baf10766040a7c35ed52d523
Kaique Oliveira <kaique.bti@gmail.com>
2023-05-05 23:05:55 -0300

    Regressão

--- lists/tests.py
+++ lists/tests.py
@@ -2,9 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    if request.method == 'POST':
-        return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
 
 def test_can_save_a_POST_request(self):
    response = self.client.post('/', data={'item_text': 'A new list item'})
